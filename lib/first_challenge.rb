c45731c1b5ef0563b812d58a6b0e2d1725128c9f
Nataliya R <n.rudenko@gmail.com>
2019-02-04 23:52:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,9 +18,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-         flavor == strawberry
-         
-         
+         delete_if flavor == strawberry
        end
      end
    end
