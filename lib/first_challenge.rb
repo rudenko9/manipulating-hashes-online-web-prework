85373acd5d4d0588ff016d9761dd8050b307ddba
Nataliya R <n.rudenko@gmail.com>
2019-02-05 00:15:43 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -23,8 +23,8 @@ def first_challenge
      end
    end
  end
-    [:favorite_icecream_flavors].values_at
-    [favorite_icecream_flavors].shift
+    :favorite_icecream_flavors.delete_if == "strawberry"
+  
   
   
 #remember to return your newly altered contacts hash!
