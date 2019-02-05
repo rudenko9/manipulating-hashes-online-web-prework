a452531692f4d698f2607ef88f865c7f44d0ce6c
Nataliya R <n.rudenko@gmail.com>
2019-02-05 00:03:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -23,7 +23,7 @@ def first_challenge
      end
    end
  end
-      contacts[:Freddy Mercury][:favorite_icecream_flavors].shift    
+    :favorite_icecream_flavors.delete_if {|key, value| key >= "strawberry"}
   
 #remember to return your newly altered contacts hash!
   contacts
