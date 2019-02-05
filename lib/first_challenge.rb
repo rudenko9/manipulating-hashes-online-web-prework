9e27a50c36da17dbac176646113933f60f16bd49
Nataliya R <n.rudenko@gmail.com>
2019-02-05 00:11:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -23,7 +23,7 @@ def first_challenge
      end
    end
  end
-    contacts[:Freddy Mercury][:favorite_icecream_flavors].values
+    contacts = [:Freddy Mercury][:favorite_icecream_flavors].values
   
   
 #remember to return your newly altered contacts hash!
