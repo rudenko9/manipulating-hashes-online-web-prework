05c21f9c76152ebf27f084a53b8d79ca7aa0b43a
Nataliya R <n.rudenko@gmail.com>
2019-02-05 01:26:22 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,13 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if value.class == Array && value.include?("strawberry")
+        value.delete_if {|flavor| flavor == "strawberry"}
+       end
+     end
+   end
 
   #remember to return your newly altered contacts hash!
   contacts
